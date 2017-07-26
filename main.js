85fcd4b2eb582e358d16b9435dc526388209cfd3
grantwbrooks <grantwbrooks@gmail.com>
2017-07-26 03:07:15 -0400

    fixed if loop

--- main.js
+++ main.js
@@ -3,22 +3,17 @@ $(document).ready(function(){
     $("img").click(function(){
         var a = $(this).attr("src");
         var b = $(this).attr("alt-src");
-        
+
         console.log("this is a",a);
         console.log("this is b",b);
 
-        if($(this).attr("src") == a){
+
             $(this).attr("src",function(){
                 $(this).attr("alt-src",function(){
                     return a;})
                 return b; 
             });
-        }
-        // else{
-        //     $(this).attr("src",function(){
-        //     return a;
-        //     });
-        // }
+
 
     });
 
